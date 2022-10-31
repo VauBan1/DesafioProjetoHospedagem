0d85e04a091e371dd5b383a5b512003d97c0d450
VauBan1 <Info.nicolasp6@gmail.com>
2022-10-31 06:39:18 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+using System.Globalization;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+hospedes.Add(new Pessoa(nome: "Nicolas", "Alves"));
+hospedes.Add(new Pessoa(nome: "Hóspede 2", "Sobrenome 1"));
+
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
+
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
